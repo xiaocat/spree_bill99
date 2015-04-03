f371d7b8857e85675c5910f1a70b63f39188e0d3
xiao.han <hanxiao0306@gmail.com>
2015-04-03 14:35:12 +0800

    支付回调，加入查询验证
    支付回调接口中，加入了查询订单的验证步骤

--- app/models/spree/gateway/bill99pay.rb
+++ app/models/spree/gateway/bill99pay.rb
@@ -1,8 +1,8 @@
 module Spree
   class Gateway::Bill99pay < Gateway
     preference :merchantAcctId, :string   #账户
-    # preference :payKey, :string  #人名币网关密钥
-    # preference :queryKey, :string  #查询密钥
+    preference :payKey, :string  #人名币网关密钥
+    preference :queryKey, :string  #查询密钥
     preference :server_public_key, :string #快钱公钥
     preference :client_private_key, :string #商户私钥
     preference :iconUrl, :string
