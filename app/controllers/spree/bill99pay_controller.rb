8c3ead5f12dd2d7bf7e739573af2329d27eb0e0f
xiao.han <hanxiao0306@gmail.com>
2015-04-21 18:05:19 +0800

    兼容api

--- app/controllers/spree/bill99pay_controller.rb
+++ app/controllers/spree/bill99pay_controller.rb
@@ -1,27 +1,26 @@
 module Spree
   class Bill99payController < StoreController
-    # ssl_allowed
     skip_before_filter :verify_authenticity_token
 
-    def bill99_url(action, options) # :nodoc: all
+    def bill99_url(action, options)
       options.reject!{|k,v| v.blank? }
       key = OpenSSL::PKey::RSA.new(payment_method.preferences[:client_private_key].gsub('\n', "\n"))
       options << ['signMsg', Base64.encode64(key.sign(OpenSSL::Digest::SHA1.new, options.map{|k,v| "#{k}=#{v}" }.join('&')))]
       cgi_escape_action_and_options(action, options)
     end
 
-    def cgi_escape_action_and_options(action, options) # :nodoc: all
+    def cgi_escape_action_and_options(action, options)
       "#{action}?#{options.sort.map{|k, v| "#{CGI::escape(k.to_s)}=#{CGI::escape(v.to_s)}" }.join('&')}"
     end
 
-    def checkout
-      order = current_order || raise(ActiveRecord::RecordNotFound)
+    def pay_option(order)
       bankId = params[:bankId] || nil
-      host = request.url.sub(request.fullpath, '')
+      host = payment_method.preferences[:returnHost].blank? ? request.url.sub(request.fullpath, '') : payment_method.preferences[:returnHost]
+      show_url = params[:redirect_url].blank? ? (host + '/products/' + order.products[0].slug) : params[:redirect_url]
 
       url = bill99_url("https://www.99bill.com/gateway/recvMerchantInfoAction.htm", [
           ["inputCharset", 1],
-          ["pageUrl", host  + '/bill99pay/notify?id=' + order.id.to_s + '&payment_method_id=' + params[:payment_method_id].to_s],
+          ["pageUrl", show_url],
           ["bgUrl", host + '/bill99pay/notify?id=' + order.id.to_s + '&payment_method_id=' + params[:payment_method_id].to_s],
           ["version", "v2.0"],
           ["language", 1],
@@ -36,8 +35,71 @@ module Spree
           ["payType", bankId ? "10" : "00"],
           ["bankId", bankId ? bankId.upcase : nil]
       ])
+    end
 
-      render json:  { 'url' => url }
+    def checkout
+      order = current_order || raise(ActiveRecord::RecordNotFound)
+      render json:  { 'url' => self.pay_option(order) }
+    end
+
+    def checkout_api
+      order = Spree::Order.find(params[:id]) || raise(ActiveRecord::RecordNotFound)
+      render json:  { 'url' => self.pay_option(order) }
+    end
+
+    def query
+      order = Spree::Order.find(params[:id]) || raise(ActiveRecord::RecordNotFound)
+
+      if order.complete?
+        render :text => "success", :layout => false
+        return
+      end
+
+      is_valid = begin
+        Timeout::timeout(10) do
+          options = [
+              ['version', 'v2.0'],
+              ['signType', 1],
+              ['merchantAcctId', payment_method.preferences[:merchantAcctId]],
+              ['queryType', 0],
+              ['queryMode', 1],
+              ['orderId', order.number]
+          ]
+          options << ['signMsg', Digest::MD5.hexdigest((options+[['key', payment_method.preferences[:queryKey]]]).map{|k,v|"#{k}=#{v}"}.join('&')).upcase]
+          result = SOAP::WSDLDriverFactory.new("https://www.99bill.com/apipay/services/gatewayOrderQuery?wsdl").create_rpc_driver.gatewayOrderQuery(options.map{|k,v| { k => { Fixnum => SOAP::SOAPInt, String => SOAP::SOAPString }[v.class].new(v) } }.inject(&:merge))
+          order_re = result.orders[0]
+          signInfo = Digest::MD5.hexdigest((%w[orderId orderAmount orderTime dealTime payResult payType payAmount fee dealId].map{|k| (v = order_re.send(k)) && v != '' ? [k, v] : nil }.compact+[['key', payment_method.preferences[:queryKey]]]).map{|k,v|"#{k}=#{v}"}.join('&')).upcase
+          signMsg = Digest::MD5.hexdigest((%w[version signType merchantAcctId errCode currentPage pageCount pageSize recordCount].map{|k| (v = result.send(k)) && v != '' ? [k, v] : nil }.compact+[['key', payment_method.preferences[:queryKey]]]).map{|k,v|"#{k}=#{v}"}.join('&')).upcase
+          if order_re.payResult == '10' && order_re.orderId == order.number && order_re.orderAmount.to_s == (order.total*100).to_i.to_s && order_re.signInfo == signInfo && result.signMsg == signMsg
+            order.payments.create!({
+              :source => Spree::Bill99PayNotify.create({
+                  :merchant_acct_id => payment_method.preferences[:merchantAcctId],
+                  :order_id => order_re.orderId,
+                  :order_amount => order_re.orderAmount,
+                  :deal_id => order_re.dealId,
+                  :pay_amount => order_re.payAmount,
+                  :fee => order_re.fee,
+                  :source_data => order_re.to_json
+              }),
+              :amount => order.total,
+              :payment_method => payment_method
+            })
+            order.next
+            true
+          else
+            false
+          end
+        end
+      rescue Exception => e
+        false
+      end
+
+      if is_valid
+        render :text => "success", :layout => false
+      else
+        render :text => "failure", :layout => false
+      end
+
     end
 
     def notify
